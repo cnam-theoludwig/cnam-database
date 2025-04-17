21df1801fe87c3af15c54870ef2d3163a311b853
BRENNER Quentin <brenner.quentin@gmail.com>
2025-04-17 15:49:41 +0200

    feat: add faker data gen

--- src/datagen.ts
+++ src/datagen.ts
@@ -1,13 +1,20 @@
 import { database } from "./database.ts"
+import { Faker, fr } from "@faker-js/faker"
+
+export const faker = new Faker({
+  locale: [fr],
+})
+faker.seed(123)
+faker.setDefaultRefDate("2024-09-15T12:00:00.000Z")
 
 database
   .insertInto("passenger")
   .values([
     {
-      first_name: "John",
-      last_name: "Doe",
-    },
+      first_name: faker.person.firstName(),
+      last_name: faker.person.lastName(),
+    }, 
   ])
   .execute()
 
-// await database.destroy()
+await database.destroy()
