34fce93b4aae501c5647766ae17a2a8d5e12aae4
BRENNER Quentin <brenner.quentin@gmail.com>
2025-04-17 16:54:20 +0200

    feat: datagen

--- src/datagen.ts
+++ src/datagen.ts
@@ -7,14 +7,81 @@ export const faker = new Faker({
 faker.seed(123)
 faker.setDefaultRefDate("2024-09-15T12:00:00.000Z")
 
-database
-  .insertInto("passenger")
-  .values([
-    {
-      first_name: faker.person.firstName(),
-      last_name: faker.person.lastName(),
-    }, 
-  ])
+const JOBS = [
+  "Pilot",
+  "Flight attendant",
+  "Copilot",
+  "Aircraft mechanic",
+  "Flight coordinator",
+  "Cabin manager",
+] as const
+const AIRPLANE_BRANDS = ["Airbus", "Boeing"]
+const AIRPLANE_MODELS = [
+  "A320",
+  "A330",
+  "A350",
+  "B737",
+  "B747",
+  "B777",
+  "B787",
+  "A380",
+  "A220",
+  "A310",
+  "A300",
+  "B767",
+  "B757",
+] as const
+
+for (let i = 0; i < 200; i++) {
+  await database
+    .insertInto("passenger")
+    .values(
+      Array.from({ length: 1000 }).map(() => {
+        return {
+          first_name: faker.person.firstName(),
+          last_name: faker.person.lastName(),
+        }
+      }),
+    )
+    .execute()
+}
+
+await database
+  .insertInto("customer")
+  .values(
+    faker.helpers.uniqueArray(faker.internet.email, 1000).map((email) => {
+      return {
+        email,
+        password: faker.internet.password(),
+      }
+    }),
+  )
   .execute()
+
+await database
+  .insertInto("employee")
+  .values(
+    Array.from({ length: 1000 }).map(() => {
+      const job = JOBS[
+        Math.floor(Math.random() * JOBS.length)
+      ] as (typeof JOBS)[number]
+      let salary = faker.number.int({ min: 1800 * 100, max: 3200 * 100 })
+      if (job === "Pilot") {
+        salary = faker.number.int({ min: 4000 * 100, max: 7500 * 100 })
+      } else if (job === "Copilot") {
+        salary = faker.number.int({ min: 3000 * 100, max: 5000 * 100 })
+      }
+      return {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        job,
+        hire_date: faker.date.past(),
+        salary_cents_euro: salary,
+      }
+    }),
+  )
+  .execute()
+
+await database.insertInto("airplane").values(Array.from({ length: 224 }))
 
 await database.destroy()
